0169e33dec0360ce4d5ed036efca47b0470650d1
Helen Chi <helenchixp@hotmail.com>
2023-11-01 13:23:26 +0900

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -99,8 +99,10 @@ public class DemoApplication extends SpringBootServletInitializer {
 			"	    let badCount = el.badCount;	\r\n" +
 			"	    let code = '<ul>'+	\r\n" +
 			"	    '<li>投稿' + title + \r\n" +
-			"	    '<input type=\"button\" value=\"いいね：' + goodCount + '\" onclick=\"alert()\">' + \r\n" +
-			"	    '<input type=\"button\" value=\"だめね：' + badCount + '\" onclick=\"alert()\"></li>' + \r\n" +
+			//"	    '<input type=\"button\" value=\"いいね：' + goodCount + '\" onclick=\"alert()\">' + \r\n" +
+			//"	    '<input type=\"button\" value=\"だめね：' + badCount + '\" onclick=\"alert()\"></li>' + \r\n" +
+			"	    '<a >いいね：' + goodCount + '</a>' + \r\n" +
+			"	    '<a >だめね：' + badCount + '</a></li>' + \r\n" +
 			"	    '</ul>';	\r\n" +
 			"	    array.insertAdjacentHTML('beforebegin', code);	\r\n" +
 			"	  });\r\n" +
